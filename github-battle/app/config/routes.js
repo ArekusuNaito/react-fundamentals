20047cf6a72bf9dbca34feb9a7ee2dc21e80d5c2
ArekusuNaito <arekusunaito@gmail.com>
2016-12-27 18:19:42 -0600

    React Router + Added Main and Home Components
    - Created 2 components.
    - Whenever the main component it just render whatever the props are
    - Configured react-router on routes.js
    - Changed react,react-dom and react-router to tutorial's version

---
+++ github-battle/app/config/routes.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../components/Main');
+var Home = require("../components/Home");
+
+// Maps URL to ceirtain components
+
+
+//Inside routes there's  JSX
+//No matter what path someone goes to, make sure Main is active
+//For example, Twitter's TitleBar/Header is always active, no matter what path we are in
+//The router tracks which children are activated
+var routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+);
+
+module.exports = routes;
--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,97 +1,14 @@
-var USER_DATA =
-{
-  name: 'Alex',
-  username: 'ArekusuNaito',
-  image: "http://pre08.deviantart.net/e357/th/pre/f/2016/195/b/7/bara_by_kuvshinov_ilya-daa1mf5.jpg"
-}
-
-// {
-//   name: 'Joy McJoyce',
-//   username: 'Joy.of',
-//   image: "http://pre08.deviantart.net/e357/th/pre/f/2016/195/b/7/bara_by_kuvshinov_ilya-daa1mf5.jpg"
-// }
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 //f(d) = V
 //data returns a View
 
-var ProfilePic = React.createClass(
-  {
-      render: function()
-      {
-        console.log(this.props);
-        return (<img src={this.props.imageURL} style={{height: 100,width: 100}} />)
-      }
-  })
-
-var ProfileLink = React.createClass(
-  {
-    render: function()
-    {
-      console.log(this.props);
-      return(
-        <div>
-          <a href={"https://github.com/"+ this.props.username} >
-            {this.props.username}
-          </a>
-        </div>
-      )
-    }
-  }
-)
-
-var ProfileName = React.createClass(
-  {
-    render: function()
-    {
-      console.log(this.props);
-      return(
-        <div>{this.props.name}</div>
-      )
-    }
-  }
-)
-
-
-
-var Avatar = React.createClass(
-  {
-    render : function()
-    {
-      console.log(this.props);
-      return(
-        <div>
-          <ProfilePic imageURL={this.props.user.image} />
-          <ProfileName name={this.props.user.name} />
-          <ProfileLink username={this.props.user.username} />
-        </div>
-      )
-    }
-  }
-)
+var routes = require('./config/routes');
+//Created 2 components.
+//Whenever the main component it just render whatever the props are
 
 ReactDOM.render
 (
-  <Avatar user={USER_DATA}/>, document.getElementById('app')
+  routes, document.getElementById('app')
 )
-
-
-//Example 1 for props
-// var HelloWorld = React.createClass(
-//   {
-//     render: function()
-//     {
-//       //Pure function
-//       console.log(this.props);
-//       return (
-//         <div> Hello {this.props.name} your favorite color is {this.props.favoriteColor} </div>
-//       )
-//     }
-//   }
-// )
-//
-// ReactDOM.render(
-//   <HelloWorld name="Joy" favoriteColor="Blue"/>, document.getElementById('app')
-// )
